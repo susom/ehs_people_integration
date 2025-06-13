94bfb06c2ce2b40857dd762d06698a33cd23b7fa
Jordan Schultz <jmschult@stanford.edu>
2025-06-12 17:49:19 -0700

    Added dynamically generated columns for statuses

--- EHSPeopleIntegration.php
+++ EHSPeopleIntegration.php
@@ -1,6 +1,8 @@
 <?php
 namespace Stanford\EHSPeopleIntegration;
 
+use DateTime;
+
 class EHSPeopleIntegration extends \ExternalModules\AbstractExternalModule {
 
     const BUILD_FILE_DIR = 'ehs-dashboard/dist/assets';
@@ -55,60 +57,115 @@ class EHSPeopleIntegration extends \ExternalModules\AbstractExternalModule {
 
         return $assets;
     }
+
     public function redcap_module_ajax($action, $payload, $project_id, $record, $instrument, $event_id, $repeat_instance,
-        $survey_hash, $response_id, $survey_queue_hash, $page, $page_full, $user_id, $group_id)
+                                       $survey_hash, $response_id, $survey_queue_hash, $page, $page_full, $user_id, $group_id)
     {
-        switch($action) {
-            case "TestAction":
-                \REDCap::logEvent("Test Action Received");
-                $result = [
-                    "success"=>true,
-                    "user_id"=>$user_id
-                ];
-                break;
-            case "getRecords":
-                $payload = json_decode($payload, true);
-                $param = [];
-                $detailsParams = [
-                    "return_format" => "json",
-                    "fields" => ["record_id", "non_type", "emp_inc_type"],
-                    "project_id" => $project_id,
-                ];
-
-                $res = json_decode(\REDCap::getData($detailsParams),true);
-                $res = $this->parseNonType($res);
-                $result = [
-                    "success"=>true,
-                    "data"=>$res
-                ];
-                break;
-            default:
-                // Action not defined
-                throw new Exception ("Action $action is not defined");
+        try {
+//            $sanitized = $this->sanitizeInput($payload);
+            return match ($action) {
+                'getRecords' => $this->getRecords($payload),
+                default => throw new Exception ("Action $action is not defined"),
+            };
+        } catch (\Exception $e) {
+            // log error
+            \REDCap::logEvent($e);
         }
-
-        // Return is left as php object, is converted to json automatically
-        return $result;
     }
 
-    function parseNonType($records) {
+    /**
+     * @param $payload
+     * @return array
+     */
+    public function getRecords($payload): array
+    {
+        $pSettings = $this->getProjectSettings();
+        $completeFields = [];
+        foreach($pSettings['status-column'] as $formName){
+            $completeFields[] = $formName . "_complete";
+        }
+        $default = ["record_id", "non_type", "emp_inc_type", "non_name", "emp_first_name", "emp_last_name", "non_date", "emp_incident_date", "tri_lead_name", "tri_lead_safety_group"];
+
+        $detailsParams = [
+            "return_format" => "json",
+            "fields" => array_merge($default, $completeFields),
+            "project_id" => $this->getProjectId(),
+        ];
+
+        $res = json_decode(\REDCap::getData($detailsParams), true);
+        $res = $this->normalizeData($res, $completeFields);
+
+        return [
+            "success" => true,
+            "data" => $res,
+            "columns" => $this->generateStatusColumns($completeFields)
+        ];
+    }
+
+    /**
+     * @param $completeFields
+     * @return array
+     */
+    public function generateStatusColumns($completeFields): array
+    {
+        $statusList = [];
+        foreach($completeFields as $field) {
+            // Parse label
+            $parts = explode('_', $field);
+            array_pop($parts); // remove 'complete'
+            $label = ucwords(implode(' ', $parts)); // e.g., "some_words" -> "Some Words"
+
+            $statusList[] = $label;
+        }
+        return $statusList;
+    }
+
+    /**
+     * @param $records
+     * @param $completeFields
+     * @return array
+     */
+    function normalizeData($records, $completeFields): array
+    {
         $nonTypeValues = [];
+        $statusList = [];
         $project = new \Project(PROJECT_ID);
         $parsed = $this->parseEnumField($project->metadata["non_type"]['element_enum']);
 
-        foreach($records as $k => $record) {
+        foreach ($records as $k => $record) {
+            $nonTypeValues = []; // move inside to reset per-record
+            $statusList = [];
+
             foreach ($record as $key => $value) {
+                // Convert non_type checkbox to list of values
                 if (preg_match('/^non_type___(\d+)$/', $key, $matches) && $value === "1") {
-                    $num = (int) $matches[1];
+                    $num = (int)$matches[1];
                     $nonTypeValues[] = $parsed[$num];
+                }
+
+                // Format date field
+                if ($key === "non_date") {
+                    $date = DateTime::createFromFormat('Y-m-d', $value);
+                    if ($date) {
+                        $records[$k]["non_date"] = $date->format('m-d-Y');
+                    }
                 }
             }
 
-//            if(isset($parsed)[]) {}
-//            if (isset($parsed[$fieldData[$variableName]])) {
-//                $value = $parsed[$fieldData[$variableName]];
-//            }
-            // Add the new 'non_type' key to the original array
+            // Build completed_statuses array with parsed labels
+
+            foreach ($completeFields as $fieldName) {
+                $statusValue = $record[$fieldName] ?? '';
+
+                // Parse label
+                $parts = explode('_', $fieldName);
+                array_pop($parts); // remove 'complete'
+                $label = ucwords(implode(' ', $parts)); // e.g., "some_words" -> "Some Words"
+
+                $statusList[$label] = $statusValue;
+            }
+
+            $records[$k]['completed_statuses'] = $statusList;
             $records[$k]['non_type_concat'] = implode(', ', $nonTypeValues);
         }
 
