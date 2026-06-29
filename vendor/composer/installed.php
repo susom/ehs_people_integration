0fd2597c117c5fc6dea6fdff9313d345729393cd
ihabz <ihab.zeedia@gmail.com>
2026-06-29 12:47:45 -0700

    fix security vulnerabilities

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '34d729ad182ffed47dae0b6f1621a9856c6fc3cf',
+        'reference' => 'e5295eb8faf9be9406b46facbac26be0703db9a9',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '34d729ad182ffed47dae0b6f1621a9856c6fc3cf',
+            'reference' => 'e5295eb8faf9be9406b46facbac26be0703db9a9',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
